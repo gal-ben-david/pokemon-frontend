e35a5aa4084895a6b3384cfe9648cabb8931aec5
Gal Ben David <gal.benda3@gmail.com>
2025-06-03 12:36:05 +0300

    Refactor data fetching to use TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { PokemonIndex } from './pages/PokemonIndex'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <div className="app-container">
-      <main className="main-content main-layout">
-        <PokemonIndex />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app-container">
+        <main className="main-content main-layout">
+          <PokemonIndex />
+        </main>
+      </div>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/PokemonIndex.tsx
+++ src/pages/PokemonIndex.tsx
@@ -1,31 +1,20 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { pokemonService } from "../services/pokemon.service"
-import type { Pokemon } from "../interfaces"
 import { PokemonList } from "../cmps/PokemonList"
+import { useQuery } from '@tanstack/react-query'
 
 
 export function PokemonIndex() {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([])
     const [visibleCount, setVisibleCount] = useState(14)
     const [isOnlyFavPoke, setIsOnlyFavPoke] = useState(false)
 
-    const loadPokemon = async () => {
-        try {
-            const fetchFn = isOnlyFavPoke
-                ? pokemonService.loadFavList
-                : pokemonService.query
-
-            const pokemon = await fetchFn()
-            setPokemons(pokemon)
-            console.log('pokemon', pokemons)
-        } catch (err) {
-            console.error('Cannot load pokemons', err)
-        }
-    }
-
-    useEffect(() => {
-        loadPokemon()
-    }, [isOnlyFavPoke])
+    const { data: pokemons = [], refetch, isLoading, isError, error } = useQuery({
+        queryKey: ['pokemons', isOnlyFavPoke],
+        queryFn: () =>
+            isOnlyFavPoke
+                ? pokemonService.loadFavList()
+                : pokemonService.query()
+    })
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setIsOnlyFavPoke(e.target.checked)
@@ -33,13 +22,16 @@ export function PokemonIndex() {
 
     const addToFavList = async (pokemonId: number) => {
         await pokemonService.add(pokemonId)
-        loadPokemon()
+        refetch()
     }
 
     const removeFromFavList = async (pokemonId: number) => {
         await pokemonService.remove(pokemonId)
-        loadPokemon()
+        refetch()
     }
+
+    if (isLoading) return <div>Loading...</div>
+    if (isError) return <div>Error: {error.message}</div>
 
     return (
         <section className="pokemon-index">
