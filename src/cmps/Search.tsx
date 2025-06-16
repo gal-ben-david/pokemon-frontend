34c766bec97ede30bd0881c0cfeb356f6d240994
Gal Ben David <gal.benda3@gmail.com>
2025-06-16 17:08:59 +0300

    Refactor debounce logic from using useMemo to useRef

--- src/cmps/Search.tsx
+++ src/cmps/Search.tsx
@@ -1,17 +1,22 @@
+import { debounce } from '../services/util.service'
+import { useRef, useState } from "react"
 
 export function Search({ filterBy, setFilterBy }: { filterBy: string, setFilterBy: (value: string) => void }) {
 
+    const [inputValue, setInputValue] = useState(filterBy)
+
+    const debouncedSetFilterBy = useRef(debounce(setFilterBy, 300))
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let value = e.target.value
-        setFilterBy(value)
+        const value = e.target.value
+        setInputValue(value)
+        debouncedSetFilterBy.current(value)
     }
-
     return (
         <input type="text"
             className="pokemon-name"
             placeholder="Search by name"
-            value={filterBy}
+            value={inputValue}
             onChange={handleChange} />
     )
 }
